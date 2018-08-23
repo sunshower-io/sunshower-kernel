6c7755e13309f814c6cfda06bc7439bf36b40c38
Josiah Haswell <josiah.haswell@gmail.com>
2018-08-23 17:05:02 -0600

    Fixing config

--- kernel-providers/kernel-wildfly/wildfly-test-plugins/wildfly-system-tests/src/main/java/io/sunshower/kernel/testplugins2/TestPluginConfig.java
+++ kernel-providers/kernel-wildfly/wildfly-test-plugins/wildfly-system-tests/src/main/java/io/sunshower/kernel/testplugins2/TestPluginConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jndi.JndiTemplate;
 
 import javax.naming.NamingException;
-import java.rmi.Naming;
 
 @Configuration
 public class TestPluginConfig {
